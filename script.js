fccc7cc2265a29b29e1a0a5cfb4d00dcdbf5768c
Erkhtuguldur <erkhtugulduriderbat@gmail.com>
2025-05-07 00:51:16 -0700

    Implement the basic logic of the game
    Now the game is playable without any issues via the console. The input filter has yet to be added.

---
+++ script.js
@@ -0,0 +1,61 @@
+let humanScore=0;
+let computerScore=0;
+
+function playGame(){
+    
+    for(i=0;i<5;i++){
+        let humanSelection = getHumanChoice();
+        let computerSelection = getComputerChoice();
+         playRound(humanSelection, computerSelection);
+    }
+    if(humanScore>computerScore){
+        console.log("You win the game");
+    }
+    else if(humanScore==computerScore){
+        console.log("It's a draw");
+    }
+    else{
+        console.log("You lose the game");
+    }
+   
+
+}
+function getComputerChoice(){
+    let value=Math.floor(Math.random()*3);
+    let str="";
+    switch(value) {
+        case 0: 
+            str="rock"; 
+            break;
+        case 1: 
+            str="paper";
+            break;
+        default: 
+            str="scissors"
+    }
+    return str;
+}
+
+function getHumanChoice(){
+    return prompt("Enter your choice");
+}
+
+function playRound(humanChoice,computerChoice){
+    humanChoice=humanChoice.toLowerCase();
+    computerChoice=computerChoice.toLowerCase();
+    console.log("Your choice: %s\nComputer's choice: %s",humanChoice,computerChoice);
+    if(humanChoice===computerChoice){
+        console.log("Draw");
+    }
+    else if((humanChoice=="rock" && computerChoice=="scissors")||(humanChoice=="paper" && computerChoice=="rock")||(humanChoice=="scissors" && computerChoice=="paper")){
+        console.log("You win the round");
+        humanScore=humanScore+1;
+    }
+    else{
+        console.log("Computer wins the round");
+        computerScore=computerScore+1;
+    }
+    console.log("Your score: %d \nComputer score:%d\n\n",humanScore,computerScore);
+}
+
+playGame();
